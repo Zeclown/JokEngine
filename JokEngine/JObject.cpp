fddac61c94b33439fc78f15ee473d8e9ec113ca9
Pierre-Olivier Chartrand <pierreolivier.chartrand@gmail.com>
2016-07-29 11:57:33 -0400

    Modified CheckDestroy Function
    Modified CheckDestroy Function so it is more concise

--- JokEngine/JObject.cpp
+++ JokEngine/JObject.cpp
@@ -13,10 +13,7 @@ namespace Jokengine
 	GLboolean JObject::CheckDestroy(GLfloat dt)
 	{
 		toDestroyTimer -= dt;
-		if (markedToDestroy && toDestroyTimer <= 0)
-		{
-			return true;
-		}
-		return false;
+		return markedToDestroy && toDestroyTimer <= 0;
+
 	}
 }
