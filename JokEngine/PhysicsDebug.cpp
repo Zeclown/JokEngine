19c332825693c4b29de2797b07d2166f96bdc683
Pierre-Olivier Chartrand <pierreolivier.chartrand@gmail.com>
2017-10-01 12:19:26 -0400

    TextRenderer Failure Handling, physics debug and Game checks
    Fixed a problem where not finding the specified font would crash the application
    Added some outputs to the physic service when in debug mode
    added some assertions to make sure every service is loaded after the game initialisation

--- JokEngine/PhysicsDebug.cpp
+++ JokEngine/PhysicsDebug.cpp
@@ -8,27 +8,32 @@ PhysicsDebug::PhysicsDebug()
 }
 glm::vec2 PhysicsDebug::GetGravity()
 {
+	std::cout << "Getting gravity X:" <<  phys->GetGravity().x << " Y:" << phys->GetGravity().y << std::endl;
 	return phys->GetGravity();
 }
 void PhysicsDebug::SetGravity(glm::vec2 gravity)
 {
+	std::cout << "Setting gravity X:" << gravity.x << " Y:" << gravity.y << std::endl;
 	phys->SetGravity(gravity);
 }
 b2Body* PhysicsDebug::RegisterBody(PhysicBody* pb,glm::vec2 position, GLboolean isKinematic, GLboolean isGravity, GLfloat rotation, GLfloat angularDrag, GLfloat drag, GLint mass, GLboolean isRotation)
 {
+	std::cout << "Registering body at position " << " X:" << position.x << " Y:" << position.y;
 	return phys->RegisterBody(pb,position, isKinematic, isGravity, rotation, angularDrag, drag, mass, isRotation);
 }
 b2Fixture* PhysicsDebug::RegisterFixtureBox(b2Body *body,Collider* col, glm::vec2 size,GLboolean sensor, glm::vec2 offset, std::string layerName)
 {
+	std::cout << "Registering FixtureBox at position " << " X:" << body->GetPosition().x << " Y:" << body->GetPosition().x;
 	return(phys->RegisterFixtureBox(body, col, size,sensor, offset, layerName));
 }
 b2Fixture* PhysicsDebug::RegisterFixtureCircle(b2Body *body, Collider* col, GLfloat radius,GLboolean sensor, glm::vec2 offset, std::string layerName)
 {
+	std::cout << "Registering FixtureCircle at position " << " X:" << body->GetPosition().x << " Y:" << body->GetPosition().x;
 	return phys->RegisterFixtureCircle(body, col,  radius, sensor, offset,  layerName);
 }
 b2Fixture* PhysicsDebug::RegisterFixtureEdge(b2Body *body, Collider* col, glm::vec2 pointA, glm::vec2 pointB, std::string layerName)
 {
-
+	std::cout << "Registering FixtureEdge at position " << " X:" << body->GetPosition().x << " Y:" << body->GetPosition().x;
 	return phys->RegisterFixtureEdge(body, col,pointA, pointB, layerName);
 }
 void PhysicsDebug::SetMaskBits(std::string layerName, std::string otherLayer, GLboolean isColliding)
